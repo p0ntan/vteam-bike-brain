961ffa8454ce775e00c55adff4a410b38168a57e
p0ntan <pontus.akerberg@hotmail.com>
2023-11-26 13:51:21 +0000

    Update sselistener, split up method

--- app/src/sselistener.py
+++ app/src/sselistener.py
@@ -26,17 +26,23 @@ class SSEListener:
         try:
             for event in SSEClient(self.URL):
                 data = json.loads(event.data)
-
-                if 'msg' in data and data['msg'] == 'start_simulation':
-                    self._bike.stop()
-                    self._bike.run_simulation()
-
-                if data['id'] == self._bike.id():
-                    print(self._bike.get_data())
-
+                self._control_bike(data)
         # Disableing pylint to catch any error, might be a bit to wide but this needs
         # to keep running for all possible errors for now.
         # TODO find out possible errors and handle them accordingly
         # pylint: disable=broad-exception-caught
         except Exception as error:
             print(f"Error in SSE connection: {error}")
+
+    def _control_bike(self, data: dict):
+        """ Control the bike with actions setn from server.
+        
+        Args:
+            data (dict): data to decide what to do with bike.
+        """
+        if 'msg' in data and data['msg'] == 'start_simulation':
+            self._bike.stop()
+            self._bike.run_simulation()
+
+        if data['id'] == self._bike.id():
+            print(self._bike.get_data())
