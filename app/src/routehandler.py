6e239812e012cb219bc924ee63eead10bf618b4c
p0ntan <pontus.akerberg@hotmail.com>
2023-11-27 10:38:39 +0000

    Updated method for checking coords

--- app/src/routehandler.py
+++ app/src/routehandler.py
@@ -12,11 +12,12 @@ class RouteHandler():
     
     Args:
         directory (str): the directory to load the routes from. Should contain .json-files.
+        interval (int): the number of seconds each interval will be, decides max-length for coords-distance
     """
-    def __init__(self, directory):
+    def __init__(self, directory, interval=10):
         """ Constructor """
         self._routes = self._load_routes(directory)
-        self._routes = self.check_coordinates()
+        self._routes = self.check_distance(interval)
 
     @property
     def routes(self):
@@ -41,31 +42,40 @@ class RouteHandler():
                     routes[bike_id] = json.load(file) # Save into dict with filename as key (id)
         return routes
 
-    def check_coordinates(self, length=50):
-        new_routes = self._routes.copy()
-        for route in new_routes.values():
-            for i, trip in enumerate(route['trips']):
-                    updated_cords = []
-                    for j, coords in enumerate(trip['coords']):
-                        try:
-                            updated_cords.append(coords)
+    def check_distance(self, interval):
+        """ Checks distance between coordiantes and adds more if needed
+        
+        Args:
+            interval (int): what interval to use for simulation in seconds    
+        """
+        max_speed_m_in_seconds = 5.5 # just under 20 km/h (19.8)
+        max_length = max_speed_m_in_seconds * interval # max length between coordinates
 
-                            next_point = [trip['coords'][j+1][0], trip['coords'][j+1][1]]
-                            coords_distance = distance(lonlat(*coords), lonlat(*next_point)).meters
-                            extra_points_needed = coords_distance / length # If more than 1 distance is too long
-                            if extra_points_needed > 1:
-                                split_by = math.ceil(extra_points_needed)
-                                lat_distance = (trip['coords'][j+1][0] - coords[0]) / split_by
-                                lng_distance = (trip['coords'][j+1][1] - coords[1]) / split_by
-                                for multiply_by in range(1, split_by):
-                                    new_lat = coords[0] + lat_distance * multiply_by
-                                    new_lng = coords[1] + lng_distance * multiply_by
-                                    updated_cords.append([new_lat, new_lng])
-                            
-                        except IndexError:
-                            # Last point in array
-                            pass
-            route['trips'][i] = updated_cords
+        new_routes = self._routes.copy()
+        for key in new_routes.keys():
+            for i, trip in enumerate(new_routes[key]['trips']):
+                updated_cords = []
+                for j, coords in enumerate(trip['coords']):
+                    try:
+                        updated_cords.append(coords)
+                        next_point = [trip['coords'][j+1][0], trip['coords'][j+1][1]]
+                        coords_distance = distance(lonlat(*coords), lonlat(*next_point)).meters
+                        extra_points_needed = coords_distance / max_length # If more than 1 distance is too long
+                        if extra_points_needed > 1:
+                            split_by = math.ceil(extra_points_needed)
+                            lat_distance = (trip['coords'][j+1][0] - coords[0]) / split_by
+                            lng_distance = (trip['coords'][j+1][1] - coords[1]) / split_by
+
+                            for multiply_by in range(1, split_by):
+                                new_lat = round(coords[0] + lat_distance * multiply_by, 6)
+                                new_lng = round(coords[1] + lng_distance * multiply_by, 6)
+                                updated_cords.append([new_lat, new_lng])
+                        
+                    except IndexError:
+                        # Last point in array
+                        pass
+                new_routes[key]['trips'][i]['coords'] = updated_cords
+    
         return new_routes
 
     # def _add_coords(self):
