29d8eb587572eeab34d91f69610830b8b1e066a6
p0ntan <pontus.akerberg@hotmail.com>
2023-12-06 23:01:29 +0000

    Rewrote to pytest, adding async tests

--- app/tests/test_bike.py
+++ app/tests/test_bike.py
@@ -2,65 +2,114 @@
 # -*- coding: UTF-8 -*-
 """ Module for testing the class Bike """
 
-import unittest
+import asyncio
+import pytest
+from unittest.mock import AsyncMock, MagicMock
 from src.bike import Bike
 from src.battery import BatterySimulator
 from src.gps import GpsSimulator
 
+bike_data = {
+    'id': 1,
+    'city_id': 'STHLM',
+    'status_id': 1,
+    'coords': [13.508699207322167, 59.38210003526896],
+}
 
-class TestBike(unittest.TestCase):
-    """ Testclass for testing class Bike """
-    BIKE_DATA = {
-        'id': 1,
-        'city_id': 'STHLM',
-        'status_id': 1,
-        'coords': [13.508699207322167, 59.38210003526896],
-    }
+def test_create_bike():
+    """ Creating a Bike and checks data """
+    gps_sim = GpsSimulator(bike_data['coords'])  # Could be mocked, but it takes away the purpose of the test
+    battery_sim = BatterySimulator()  # Could/should be mocked, but then takes away the purpose of the test
 
-    def test_create_bike(self):
-        """ Creating a Bike and checks data """
-        gps_sim = GpsSimulator(self.BIKE_DATA['coords'])
-        battery_sim = BatterySimulator()
+    bike_instance = Bike(bike_data, battery_sim, gps_sim)
+    data_from_bike = bike_instance.get_data()
+    assert isinstance(data_from_bike, dict)
 
-        bike_instance = Bike(self.BIKE_DATA, battery_sim, gps_sim)
-        data_from_bike = bike_instance.get_data()
-        self.assertIsInstance(data_from_bike, dict, "Should be a dictionary")
-        # Check that data has all the right keys and values
-        # id
-        self.assertIn('id', data_from_bike, "Should have key id")
-        self.assertIsInstance(data_from_bike['id'], int, "Should be an integer")
-        # status_id
-        self.assertIn('status_id', data_from_bike, "Should have key status")
-        self.assertIsInstance(data_from_bike['status_id'], int, "Should be an integer")
-        # charge_perc
-        self.assertIn('charge_perc', data_from_bike, "Should have key charge_perc")
-        self.assertIsInstance(data_from_bike['charge_perc'], float, "Should be a float")
-        # coords
-        self.assertIn('coords', data_from_bike, "Should have key coords")
-        self.assertIsInstance(data_from_bike['coords'], list, "Should be a list")
-        self.assertIsInstance(data_from_bike['coords'][0], float, "Should be a float")
-        self.assertIsInstance(data_from_bike['coords'][1], float, "Should be a float")
-        # speed
-        self.assertIn('speed', data_from_bike, "Should have key speed")
-        self.assertIsInstance(data_from_bike['speed'], int, "Should be an integer")
+    # Check that data has all the right keys and values
+    # id
+    assert 'id' in data_from_bike
+    assert isinstance(data_from_bike.get('id'), int)
+    # status_id
+    assert 'status_id' in data_from_bike
+    assert isinstance(data_from_bike.get('status_id'), int)
+    # charge_perc
+    assert 'charge_perc' in data_from_bike
+    assert isinstance(data_from_bike.get('charge_perc'), float)
+    # coords
+    assert 'coords' in data_from_bike
+    assert isinstance(data_from_bike.get('coords'), list)
+    assert isinstance(data_from_bike.get('coords')[0], float)
+    assert isinstance(data_from_bike.get('coords')[1], float)
+    # speed
+    assert 'speed' in data_from_bike
+    assert isinstance(data_from_bike.get('speed'), int)
 
-    def test_moving_bike(self):
-        """ Update position for a bike, see that it sends out speed and new position """
-        gps_sim = GpsSimulator(self.BIKE_DATA['coords'])
-        battery_sim = BatterySimulator()
-        bike_instance = Bike(self.BIKE_DATA, battery_sim, gps_sim)
+def test_moving_bike():
+    """ Update position for a bike, see that it sends out speed and new position """
+    gps_sim = GpsSimulator(bike_data['coords'])  # Could be mocked, but it takes away the purpose of the test
+    battery_sim = MagicMock()
+    bike_instance = Bike(bike_data, battery_sim, gps_sim)
 
-        data_from_bike = bike_instance.get_data()
-        self.assertEqual(data_from_bike['speed'], 0, "Should be 0")
-        self.assertEqual(data_from_bike['coords'], self.BIKE_DATA['coords'], "Should be an list of coordinates")
+    data_from_bike = bike_instance.get_data()
+    assert data_from_bike.get('speed') == 0
+    assert data_from_bike.get('coords') == bike_data.get('coords')
 
-        new_position = [13.505173887431198, 59.38216072603788]  # 200 meters between this and original position
-        interval_in_seconds = 10
+    new_position = [13.505173887431198, 59.38216072603788]  # 200 meters between this and original position
+    interval_in_seconds = 10
 
-        # Forcing new position on private variable, disableing pylint for this action
+    # Forcing new position on private variable, disableing pylint for this action
+    # pylint: disable=protected-access
+    bike_instance._gps.position = (new_position, interval_in_seconds)
+    data_from_bike = bike_instance.get_data()
+
+    assert data_from_bike.get('speed') == 72  # 200 meters in 10 sek = 72 km/h
+    assert data_from_bike.get('coords') == new_position
+
+@pytest.mark.asyncio
+async def test_start_bike_low_battery():
+    """ Test to see if status changes if battery gets to low """
+    gps_sim = MagicMock()
+    battery_sim = MagicMock()
+    battery_sim.needs_charging = MagicMock(side_effect=[False, False, True, True])
+
+    bike = Bike(bike_data, battery_sim, gps_sim)
+
+    # Mock methods to isolate test
+    # pylint: disable=protected-access
+    bike._update_bike_data = AsyncMock()
+    bike._used_interval = 2
+    bike._running = True
+    bike._running_simulation = False
+
+    async def stop_bike_after_5_seconds():
+        await asyncio.sleep(5)
         # pylint: disable=protected-access
-        bike_instance._gps.position = (new_position, interval_in_seconds)
-        data_from_bike = bike_instance.get_data()
+        bike._running = False
+    
+    # Check that status is 1 when starting bike
+    assert bike.status == 1
 
-        self.assertEqual(data_from_bike['speed'], 72, "Should be 72")  # 200 meters in 10 sek = 72 km/h
-        self.assertEqual(data_from_bike['coords'], new_position, "Should be an list of coordinates")
+    # Run bike.start() and stop after 5 seconds
+    await asyncio.gather(
+        bike.start(),
+        stop_bike_after_5_seconds()
+    )
+
+    # Control that needs_charging has run three times and status has changed
+    assert bike._battery.needs_charging.call_count == 3
+    assert bike.status == 3
+
+def test_change_status_when_low_battery():
+    """ Trying to change status when battery i low. """
+    gps_sim = MagicMock()
+    battery_sim = MagicMock()
+    battery_sim.needs_charging = MagicMock(return_value=True)
+
+    bike = Bike(bike_data, battery_sim, gps_sim)
+    assert bike.status == 1
+
+    bike.set_status(3)
+    assert bike.status == 3
+
+    bike.set_status(1)  # Should stay at 3 since low battery
+    assert bike.status == 3
