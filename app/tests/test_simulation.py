90f90b2df87f647e83aff1cdf381d2db47e034f2
p0ntan <pontus.akerberg@hotmail.com>
2023-12-20 08:11:53 +0000

    Change test using two simulations, and only 5 sec interval

--- app/tests/test_simulation.py
+++ app/tests/test_simulation.py
@@ -21,54 +21,9 @@ def mock_bike_methods():
         yield mock_start_renting, mock_update_data, mock_end_renting
 
 
-@pytest.mark.asyncio
-async def test_simulation_default(mock_bike_methods):
-    """ Test to run the simulation, with default interval (10 seconds). """
-    mock_start_renting, mock_update_data, mock_end_renting = mock_bike_methods
-    base_dir = os.path.dirname(__file__)
-    test_directory = os.path.join(base_dir, 'test-data')
-    bike_data = {
-        'id': 1,
-        'city_id': "KSD_TEST",
-        'status_id': 1,
-        'coords': [13.49476285318039, 59.3790176201388]
-    }
-
-    r_handler = RouteHandler(test_directory)
-    sim_route = r_handler.routes.get(bike_data.get('id'))
-    sim_gps = GpsSimulator(bike_data.get('coords'))
-    sim_battery = BatterySimulator()
-    bike = Bike(bike_data, sim_battery, sim_gps, sim_route)
-
-    # Adding side_effects to change status, this is done from the server
-    # through an SSE in the simulation.
-    def start_renting_side_effect(*args, **kwargs):
-        bike.set_status(2)
-        return True, 1
-
-    def end_renting_side_effect(*args, **kwargs):
-        bike.set_status(1)
-
-    mock_start_renting.side_effect = start_renting_side_effect
-    mock_end_renting.side_effect = end_renting_side_effect
-
-    await bike.run_simulation()
-
-    mock_start_renting.assert_called_once()
-    mock_end_renting.assert_called_once()
-
-    data_args = mock_update_data.call_args_list
-    for arg in data_args:
-        test, _ = arg
-        speed = test[0].get('speed')
-        assert speed <= 20
-
-
-@pytest.mark.asyncio
-async def test_simulation_five(mock_bike_methods):
-    """ Test to run the simulation, with five second interval. """
+@pytest.fixture
+def bike_setup_five():
     interval = 5
-    mock_start_renting, mock_update_data, mock_end_renting = mock_bike_methods
     base_dir = os.path.dirname(__file__)
     test_directory = os.path.join(base_dir, 'test-data')
     bike_data = {
@@ -84,6 +39,15 @@ async def test_simulation_five(mock_bike_methods):
     sim_battery = BatterySimulator()
     bike = Bike(bike_data, sim_battery, sim_gps, sim_route, interval)
 
+    return bike
+
+
+@pytest.mark.asyncio
+async def test_simulation_five(bike_setup_five, mock_bike_methods):
+    """ Test to run the simulation, with five second interval. """
+    mock_start_renting, mock_update_data, mock_end_renting = mock_bike_methods
+    bike = bike_setup_five
+
     # Adding side_effects to change status, this is done from the server
     # through an SSE in the simulation.
     def start_renting_side_effect(*args, **kwargs):
@@ -98,11 +62,29 @@ async def test_simulation_five(mock_bike_methods):
 
     await bike.run_simulation()
 
-    mock_start_renting.assert_called_once()
-    mock_end_renting.assert_called_once()
+    mock_start_renting.assert_called()
+    mock_end_renting.assert_called()
 
     data_args = mock_update_data.call_args_list
-    for arg in data_args:
-        test, _ = arg
-        speed = test[0].get('speed')
+    for arg_tuple in data_args:
+        arg, _ = arg_tuple
+        speed = arg[0].get('speed')
         assert speed <= 20
+
+
+@pytest.mark.asyncio
+async def test_simulation_error_response(bike_setup_five, mock_bike_methods):
+    """ Test to run the simulation, with error from server when starting trip. """
+    mock_start_renting, mock_update_data, mock_end_renting = mock_bike_methods
+    bike = bike_setup_five
+
+    def start_renting_side_effect(*args, **kwargs):
+        return False, None
+
+    mock_start_renting.side_effect = start_renting_side_effect
+
+    await bike.run_simulation()
+
+    mock_start_renting.assert_called()
+    mock_end_renting.assert_not_called()
+    mock_update_data.assert_not_called()
